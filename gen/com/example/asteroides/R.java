1a4faea97aa5b398512f47de2c783f029c250f11
eduardohc <lalohernandez93@gmail.com>
2014-10-16 10:39:34 -0500

    Modificacion de estilos

--- gen/com/example/asteroides/R.java
+++ gen/com/example/asteroides/R.java
@@ -65,5 +65,7 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f050001;
+        public static final int tituloAplicacion=0x7f050002;
+        public static final int tituloAplicacion_Botones=0x7f050003;
     }
 }
